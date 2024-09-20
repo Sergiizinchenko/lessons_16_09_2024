9f55c78a8af9ec23e09c3073d9e41544d7dbc407
Данило <vladnok@ukr.net>
2024-09-20 21:28:31 +0300

    Home Work 2.2

--- Home_Work_2.2.py
+++ Home_Work_2.2.py
@@ -2,22 +2,10 @@ from unicodedata import digit
 
 user_input = input("Введіть 5-ти значне число:")
 number = int(user_input)
-digit_5 = number%10
-digit_4 = (number//10)%10
-digit_3 = (number//100)%10
-digit_2 = (number//1000)%10
-#digit_1 = number//10000
-num= divmod(number,10000)
+digit_1 = divmod(number,10000)
+digit_2 = divmod(digit_1[1],1000)
+digit_3 = divmod(digit_2[1],100)
+digit_4 = divmod(digit_3[1],10)
+digit_5 = divmod(digit_4[1],1)
+print (digit_5[0],digit_4[0],digit_3[0],digit_2[0],digit_1[0],sep="")
 
-igit_1 = num[0]
-print (digit_5,digit_4,digit_3,digit_2,digit_1,sep="")
-
-#num = int(user_input)
-#pal = 0
-#while num != 0:
-#    use = divmod(num, 10)
-##    dig = use[1]
-#   pal = pal*10+dig
-#    num = use[0]
-
-#print(pal)
