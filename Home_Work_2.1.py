a9180ae925456221b76ddae5766035596eed3fd4
Данило <vladnok@ukr.net>
2024-09-20 15:10:25 +0300

    Home Work 2.1

--- Home_Work_2.1.py
+++ Home_Work_2.1.py
@@ -4,7 +4,8 @@ digit_4 = number%10
 digit_3 = (number//10)%10
 digit_2 = (number//100)%10
 digit_1 = number//1000
-print (digit_1)
-print (digit_2)
-print (digit_3)
-print (digit_4)
+print (digit_1,digit_2,digit_3,digit_4,sep='\n')
+#print (digit_1)
+#print (digit_2)
+#print (digit_3)
+#print (digit_4)
