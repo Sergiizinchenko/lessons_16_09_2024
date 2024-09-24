7dc821550051b5a47c5a490c5c3f8928a76c0fd9
Данило <vladnok@ukr.net>
2024-09-24 14:31:30 +0300

    Home Work 3.1

--- Home_Work _3.1.py
+++ Home_Work _3.1.py
@@ -1,15 +1,34 @@
-user_input = input("Введіть 5-ти значне число:")
+user_input = input("Введіть вираз:")
 number = list(user_input)
-element = '+'
-#num_a = 17
-#if not num_a > 20:
-#        print("Yes")
-#else:
-#        print ("No")
-if element in number:
-    ele=element[i]
-    num_1 = int(ele[i-1])
-    num_2 = int(ele[i+1])
-    print("+входить в список")
-else:
-    print("+ не входить в список")
+element_1 = '+'
+element_2 = '-'
+element_3 = '*'
+element_4 = '/'
+
+if element_1 in number:
+        ele= number.index(element_1)
+        ele_2 = int(ele+1)
+        num_1 = float(''.join(number[:ele]))
+        num_2 = float(''.join(number[ele_2:]))
+        print("+входить в список",num_1+num_2)
+if element_2 in number:
+        ele= number.index(element_2)
+        ele_2 = int(ele+1)
+        num_1 = float(''.join(number[:ele]))
+        num_2 = float(''.join(number[ele_2:]))
+        print("- входить в список", num_1 - num_2)
+if element_3 in number:
+        ele= number.index(element_3)
+        ele_2 = int(ele+1)
+        num_1 = float(''.join(number[:ele]))
+        num_2 = float(''.join(number[ele_2:]))
+        print("- входить в список", num_1*num_2)
+if element_4 in number:
+        ele= number.index(element_4)
+        ele_2 = ele+1
+        num_1 = float(''.join(number[:ele]))
+        num_2 = float(''.join(number[ele_2:]))
+        if num_2 == 0:
+            print("Помилка")
+        else:
+            print("- входить в список", num_1/num_2)
