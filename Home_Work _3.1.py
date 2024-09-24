1736f2e3a2adacaa6c8ca23a0e08ae6d469b0825
Данило <vladnok@ukr.net>
2024-09-24 11:33:50 +0300

    Home Work 3.1

---
+++ Home_Work _3.1.py
@@ -0,0 +1,15 @@
+user_input = input("Введіть 5-ти значне число:")
+number = list(user_input)
+element = '+'
+#num_a = 17
+#if not num_a > 20:
+#        print("Yes")
+#else:
+#        print ("No")
+if element in number:
+    ele=element[i]
+    num_1 = int(ele[i-1])
+    num_2 = int(ele[i+1])
+    print("+входить в список")
+else:
+    print("+ не входить в список")
