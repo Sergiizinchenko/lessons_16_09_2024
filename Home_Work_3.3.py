d8e2c0e34830322aa6a98181cfac5b6f57ba5269
Данило <vladnok@ukr.net>
2024-09-25 19:44:35 +0300

    Home Work 3.3_finish

--- Home_Work_3.3.py
+++ Home_Work_3.3.py
@@ -1,13 +1,13 @@
-lst_1 = [1,2,3]
-colindex_1 = len(lst_1)
-popolam_1 = int (colindex_1 / 2)
+new_lst = []
+col_index = len(new_lst)
+popolam_1 = int (colindex_1 / 2) #про
 zal_dil= colindex_1 % 2  #залишок від ділення
 if zal_dil == 0:
     new_list_1 = lst_1[:popolam_1]
     new_list_2 = lst_1[popolam_1:]
     print(popolam_1, [new_list_1, new_list_2])
 else:
-    new_list_1 = lst_1[:popolam_1]
-    new_list_2 = lst_1[popolam_1:]
-    print([new_list_1,new_list_2])
-#print (lst_1)
+    new_list_1 = lst_1[:(popolam_1+1)]
+    new_list_2 = lst_1[popolam_1+1:]
+    print(popolam_1 ,[new_list_1,new_list_2])
+
