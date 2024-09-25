2c6fff7e79a02ef9653ff06b3a424579402d6361
Данило <vladnok@ukr.net>
2024-09-25 19:56:53 +0300

    Home Work 3.3_finish_1

--- Home_Work_3.3.py
+++ Home_Work_3.3.py
@@ -1,13 +1,18 @@
-new_lst = []
-col_index = len(new_lst)
-popolam_1 = int (colindex_1 / 2) #визначаємо на скільки ділемо список
-zal_dil= colindex_1 % 2  #визначаємо залишок від ділення
-if zal_dil == 0:
-    new_list_1 = lst_1[:popolam_1]
-    new_list_2 = lst_1[popolam_1:]
+#lst = [1, 2, 3, 4, 5, 6]
+#lst = [1, 2, 3]
+lst = [1, 2, 3, 4, 5]
+#lst = [1]
+#lst = []
+
+col_index = len(lst) #кількість елементів у списку
+popolam_1 = int (col_index / 2) #визначаємо на скільки ділемо список
+zal_dil= col_index % 2  #визначаємо залишок від ділення
+if zal_dil == 0:   # якщо залишок 0 то кількість елементів парна
+    new_list_1 = lst [:popolam_1]
+    new_list_2 = lst [popolam_1:]
     print(popolam_1, [new_list_1, new_list_2])
-else:
-    new_list_1 = lst_1[:(popolam_1+1)]
-    new_list_2 = lst_1[popolam_1+1:]
+else: #кількість елементів не парна
+    new_list_1 = lst [:(popolam_1+1)]
+    new_list_2 = lst [popolam_1+1:]
     print(popolam_1 ,[new_list_1,new_list_2])
 
