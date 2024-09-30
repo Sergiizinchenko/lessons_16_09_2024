add2e823266ed8ee77658484c8600851bd84cdaa
Данило <vladnok@ukr.net>
2024-09-30 11:21:40 +0300

    Home Work 4.2_finisch

--- Home_Work_4.2.py
+++ Home_Work_4.2.py
@@ -1,12 +1,12 @@
-# first_list = [0, 1, 7, 2, 4, 8]
-# first_list =[1, 3, 5]
+#first_list = [0, 1, 7, 2, 4, 8]
+first_list =[1, 3, 5]
 # first_list =[6]
-first_list = []
+#first_list = []
 odd_list = []  # створення  нового пустого  списка
 if first_list:
     end_element = first_list[-1]  # визначення значення останнього елемента
 else:
-    end_element = 0
+    end_element = 0#отримуємо значення 0, якщо список пустий
 for element in first_list:
     if first_list.index(element) % 2 == 0:
         odd_list.append(first_list[first_list.index(element)])
