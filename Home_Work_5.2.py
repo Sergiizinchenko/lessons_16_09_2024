e427067194bf3adbf5da7273d8b49efe06a98f3c
Данило <vladnok@ukr.net>
2024-10-03 18:55:04 +0300

    Home Work 5.2_new

--- Home_Work_5.2.py
+++ Home_Work_5.2.py
@@ -1,12 +1,8 @@
 while True:
-
-    # Ваш код тут
-    # if my_list.lower() != 'Yes':
     first_symbol = float(input("1st num:  "))
     my_operator = input("operator (+, -, /, *):  ")
     second_symbol = float(input("2nd num:  "))
-    if my_operator not in ("+", "-", "*", "/"):
-        result = "ERROR - wrong operator"
+    result = None
     if my_operator == "+":
         result = first_symbol + second_symbol
     elif my_operator == "-":
@@ -18,8 +14,7 @@ while True:
             result = first_symbol / second_symbol
         else:
             result = "ERROR - can't divide by zero"
-    else:
-        result = "ERROR - wrong operator"
-    умова = input("Введіть 'так', щоб повернутися на початок програми: ")
-    if умова.lower() != 'yes':
+    print(result)
+    umova = input("Введіть 'так', щоб повернутися на початок програми: ")
+    if umova.lower() not in ('yes', "Y", "y", "ye"):
         break
