46f4342ec128dbaa36b51ba05d4af61d64da74bc
Данило <vladnok@ukr.net>
2024-10-06 15:26:00 +0300

    Home Work 6.2_finish

--- Home_Work_6.2.py
+++ Home_Work_6.2.py
@@ -2,7 +2,7 @@ t = ("день,", "днів,", "дні,",)
 my_input = int(466289)
 day, left_1 = divmod(my_input, 86400)
 god, left_2 = divmod(left_1, 3600)
-hv, left_3 = divmod(left_2, 60)
+hv,  left_3 = divmod(left_2, 60)
 god_1 = str(god)
 god_2 = god_1.zfill(2)
 #if god < 12:
