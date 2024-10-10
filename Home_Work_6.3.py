40ecd9bbe16c6da7c83269d681617b476bf76b8b
Данило <vladnok@ukr.net>
2024-10-10 08:31:15 +0300

    Home Work 6.3_new

---
+++ Home_Work_6.3.py
@@ -0,0 +1,28 @@
+my_str = str(input("Введіть число без коми:  "))
+
+my_str_new = my_str
+
+my_str = int(my_str)
+
+if my_str <= 9:
+
+    print(my_str)
+
+else:
+
+    # my_str = int(my_str)
+
+    while my_str >= 10:
+
+        # print(my_str)
+
+        res = int(my_str_new[0])
+
+        for ele in my_str_new[1:]:
+            res *= int(ele)
+
+            my_str = res
+
+            my_str_new = str(res)
+
+    print(res)
