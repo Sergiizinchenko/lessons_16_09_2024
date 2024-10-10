73dff8c1423ce396a4c60cdc21fdf566c9c5678a
Данило <vladnok@ukr.net>
2024-10-10 08:54:39 +0300

    Home Work 7.3_new

--- Home_Work_7.3.py
+++ Home_Work_7.3.py
@@ -1,5 +1,12 @@
 def second_index(text, some_str):
-  pass
+  if len(some_str) >=2:
+      ele = some_str
+      i: int = text.index(ele)
+      new_text = (text[:i]+text[i+1:])
+  while:
+        i: int = text.index(some_str)
+         new_text = (text[:i] + text[i + 1:])
+  return new_text.index(ele)
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
 assert second_index("hi", "h") is None, 'Test3'
---
+++ Home_Work_7.4.py
@@ -0,0 +1,11 @@
+
+
+
+def common_elements():
+   import random
+my_list_1 =()
+my_list_1 = [random.randint(1,100) for i in range(random.randint(3, 10))]
+
+
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
