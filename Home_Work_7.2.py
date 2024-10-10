f90cdd1a2a3fdfbdcbb21bfddcea9d37b839f3c8
Данило <vladnok@ukr.net>
2024-10-10 08:35:55 +0300

    Home Work 7.2._new

---
+++ Home_Work_7.2.py
@@ -0,0 +1,23 @@
+def correct_sentence(text):
+    if '.' in text[len(text) - 1]:
+
+        return text[0].upper() + text[1:]
+
+
+
+    else:
+
+        return text[0].upper() + text[1:] + '.'
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+
+assert correct_sentence("hello") == "Hello.", 'Test2'
+
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
+print('ОК')
