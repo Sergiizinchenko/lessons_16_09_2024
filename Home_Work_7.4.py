e3db1bbca23e796cb579ed3e342eec2617236b1d
Данило <vladnok@ukr.net>
2024-10-13 19:34:11 +0300

    Home Work 7.4_new

--- Home_Work_7.4.py
+++ Home_Work_7.4.py
@@ -1,11 +1,8 @@
-
-
-
 def common_elements():
-   import random
-my_list_1 =()
-my_list_1 = [random.randint(1,100) for i in range(random.randint(3, 10))]
-
-
-
+ my_list_one = list(range(0, 100, 3))
+ my_list_wan = list(range(0, 100, 5))
+ return set(el for el in my_list_one if el in my_list_wan)
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+print('ОК')
+
+
