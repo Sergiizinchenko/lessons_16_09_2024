a3be47646c965125f4f1656d38570e987dbcd7ea
Данило <vladnok@ukr.net>
2024-10-17 11:20:40 +0300

    Home Work 9.1_finish

--- Home_Work_9.1.py
+++ Home_Work_9.1.py
@@ -1,14 +1,9 @@
 def popular_words(text, words):
     from collections import Counter
-
     counter = dict(Counter(text.lower().split()).most_common())
-
     return {key: counter.get(key, 0) for key in words}
-
-
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
                      ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
-
 print('OK')
 
 
