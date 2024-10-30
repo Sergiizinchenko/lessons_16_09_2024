5ff15e211a9adddc353468159a365cffee77cdee
Данило <vladnok@ukr.net>
2024-10-30 13:35:19 +0200

    Home Work_11.3_finish

--- Home_Work_11.1.py
+++ Home_Work_11.1.py
@@ -1,8 +1,6 @@
 def prime_generator(end):
-    """Генерує прості числа до заданої межі, використовуючи yield."""
 
     for num in range(2, end + 1):
-
         if is_prime(num):
             yield num
 
@@ -30,48 +28,24 @@ def is_prime(n):
 
     if n <= 1:
         return False
-
     if n <= 3:
         return True
-
     if n % 2 == 0 or n % 3 == 0:
         return False
-
     i = 5
-
     while i * i <= n:
-
         if n % i == 0 or n % (i + 2) == 0:
             return False
-
         i += 6
-
     return True
-
 
 from inspect import isgenerator
 
 gen = prime_generator(1)
 
-print(gen)
-
 assert isgenerator(gen) == True, 'Test0'
-
 assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
-
 assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
-
 assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
-
 print('Ok')
 
-# Приклад використання: Генерація простих чисел до 100
-
-# for prime in prime_generator(10):
-
-#     print(prime)
-
-
-# for prime in prime_generator(10):
-
-#     print(prime)
