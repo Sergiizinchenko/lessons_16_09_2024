f2700a52e9640f25fae7f2463c5f1b2c123590c5
Данило <vladnok@ukr.net>
2024-11-07 10:09:35 +0200

    Home Work_13.1_new

--- Home_work_13.1.py
+++ Home_work_13.1.py
@@ -1,68 +1,72 @@
-def __init__(self, current=1, min_value=0, max_value=10):
-    self.current = current
-    self.min_value = min_value
-    self.max_value = max_value
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+    def __str__(self):
+        return f"{self.gender},{self.age},{self.first_name},{self.last_name}"
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{self.gender},{self.age},{self.first_name},{self.last_name},{self.record_book}"
 
 
-def set_current(self, start):
-    self.current = start
-    print(start)
+class Group:
 
-def set_max(self, max_max):
-    self.max_value = max_max
-    return self.max_value
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+
+        if last_name in self.group:
+            del self.group[last_name]
+
+            # print(f"Student {last_name} has been deleted.")
+
+        else:
+            return None  # print(f"Student {last_name} not found.")
+
+    # @classmethod
+
+    def find_student(self, last_name):
+
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+            # print(last_name)
+        return None
+
+    def __str__(self):
+
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
 
 
-def set_min(self, min_min):
-    self.min_value = min_min
-    return self.min_value
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
 
-def step_up(self):
-    if self.current < self.max_value:
-        # print(self.max_value)
-        self.current += 1
-        print(self.current)
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
 
-    else:
-        raise ValueError("Достигнут максимум")
+print(gr)
 
-
-def step_down(self):
-    if self.current > self.min_value:
-        self.current -= 1
-        print(self.current)
-    else:
-        raise ValueError("Достигнут минимум")
-
-
-def get_current(self):
-    return self.current
-
-counter = Counter()
-counter.set_current(7)
-counter.step_up()
-counter.step_up()
-counter.step_up()
-
-assert counter.get_current() == 10, 'Test1'
-
-try:
-    counter.step_up()  # ValueError
-except ValueError as e:
-    print(e)  # Достигнут максимум
-
-assert counter.get_current() == 10, 'Test2'
-counter.set_min(7)
-counter.step_down()
-counter.step_down()
-counter.step_down()
-
-assert counter.get_current() == 7, 'Test3'
-try:
-    counter.step_down()  # ValueError
-except ValueError as e:
-    print(e)  # Достигнут минимум
-
-assert counter.get_current() == 7, 'Test4'
-
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+gr.delete_student('Taylor')  # No error!
 
